8f9325f584268958bbf07b5d55edd28077ffd6b6
Harsh Goel <69888770+Harshgoel189@users.noreply.github.com>
2024-01-14 10:56:34 +0000

    datatypes

---
+++ demo/datatypes.js
@@ -0,0 +1,14 @@
+"use strict";//treat all js code into newer version
+
+// alert("Hello"); //we are using nodejs,not browser
+
+let age=22
+let myname="Harsh"
+
+let isLogIn=false
+let state;//undefined
+
+console.log(typeof age)
+console.log(typeof myname)
+console.log(typeof undefined)
+console.log(typeof null)
