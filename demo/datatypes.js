1e39b717468d49f6a5c409abc3c85e29ced2b8b4
Harsh Goel <69888770+Harshgoel189@users.noreply.github.com>
2024-01-14 11:59:20 +0000

    Conversion of datatypes

--- demo/datatypes.js
+++ demo/datatypes.js
@@ -12,3 +12,21 @@ console.log(typeof age)
 console.log(typeof myname)
 console.log(typeof undefined)
 console.log(typeof null)
+
+let num="23.12f"
+let num2=Number(num)
+console.log(typeof num2)//shows type is number
+console.log(num2)//if number is invalid then shows NaN
+
+let x=100
+let y=String(x)
+console.log(typeof x)
+
+/*
+"33" => 33
+"33abc" => NaN
+conversion types:
+        let num=Number(x)
+        let str=String(x)
+        let isBool=Boolean(x)
+*/
