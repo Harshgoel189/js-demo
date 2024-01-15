257870ae207d40434ff23f3d19e696084dd88d14
Harsh Goel <69888770+Harshgoel189@users.noreply.github.com>
2024-01-15 15:19:48 +0000

    String Methods

---
+++ demo/string.js
@@ -0,0 +1,14 @@
+let myName=new String('HarshGoel-ShivanshGoel-Shiv')
+console.log(myName.includes('hoe'));
+console.log(myName.replaceAll('e','E'));
+console.log(myName.split('-'));
+console.log(myName.split('-',2));
+console.log(typeof '');
+console.log(typeof String);
+console.log(typeof 45);
+console.log(typeof Number);
+
+let name="Harsh Goel"
+let age=22
+
+console.log(`My name is ${name} and I am ${age} years old.`);
