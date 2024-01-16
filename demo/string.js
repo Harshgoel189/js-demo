ab15654f90671c55cd8b90581fafec4411b92869
Harsh Goel <69888770+Harshgoel189@users.noreply.github.com>
2024-01-16 09:55:18 +0000

    added functions of strings

--- demo/string.js
+++ demo/string.js
@@ -1,14 +1,22 @@
 let myName=new String('HarshGoel-ShivanshGoel-Shiv')
-console.log(myName.includes('hoe'));
-console.log(myName.replaceAll('e','E'));
-console.log(myName.split('-'));
-console.log(myName.split('-',2));
-console.log(typeof '');
-console.log(typeof String);
-console.log(typeof 45);
-console.log(typeof Number);
+// console.log(myName.includes('hoe'));
+// console.log(myName.replaceAll('e','E'));
+// console.log(myName.split('-'));
+// console.log(myName.split('-',2));
+// console.log(typeof '');
+// console.log(typeof String);
+// console.log(typeof 45);
+// console.log(typeof Number);
 
 let name="Harsh Goel"
 let age=22
 
-console.log(`My name is ${name} and I am ${age} years old.`);
+// console.log(`My name is ${name} and I am ${age} years old.`);
+
+let str1="Hello My name is Sam. I am from chicago. I have done Btech.I am an avid coder."
+console.log(str1.split(' '));
+console.log(str1.includes(". I am "))
+console.log(str1.substring(2,7))
+console.log(`Hi My details are here \n${str1}`)
+console.log(str1.replaceAll(" ",'-'));
+console.log(str1.replace(".",' ?'));
