e9fdef125b88d212f07cf513b1b2c953c8367899
Harsh Goel <69888770+Harshgoel189@users.noreply.github.com>
2024-01-16 13:45:57 +0000

    Number and Maths

---
+++ demo/numAndMath.js
@@ -0,0 +1,29 @@
+const num1=100
+// console.log(num1);
+
+const num2=new Number(579.29)
+// console.log(num2);
+
+// console.log(num2.toString().length)
+// console.log(num2.toFixed(1));
+// console.log(num2.toPrecision(1));
+// console.log(num2.toPrecision(2));
+// console.log(num2.toPrecision(3));
+// console.log(num2.toPrecision(4));
+// console.log(num2.toPrecision(5));
+// console.log(num2.toPrecision(6));
+
+let balance=1000000
+// console.log(balance.toLocaleString())
+// console.log(balance.toLocaleString('en-IN'))
+
+
+
+//+++++++++++++++++++++++++++  MATH  ++++++++++++++++++++++++++++++++++++++++++++
+
+
+console.log(Math.floor(Math.random()*50)+100)//generate random value between 100 and 150
+console.log(Math.ceil(100.9));
+console.log(Math.floor(100.9));
+
+// console.log(Math.floor(Math.random() * (max-min))+min);
